f2bb79aa8f82ad6414dd24b7ab4009176e15d52c
Michael Lazos <Michael_Lazos@brown.edu>
2015-05-06 19:51:56 -0400

    added basic semantic model

--- Models/RubyCoreSemantics.rkt
+++ Models/RubyCoreSemantics.rkt
@@ -2,72 +2,96 @@
 (require redex)
 
 (define-language ruby-core
-  (e 
-     number
-     string
-     boolean
-     empty
-     (do e ...)
+  ;atomic expressions
+  (ae number
+      string 
+      boolean
+      empty
+      (list e ... empty)
+      (lam (x ...) e)
+      (proc (x ...) e)
+      x)
+  ;complex expressions
+  (ce (do e ...)
      (+ e ...)
-     (list e ... empty)
      (if e e e)
-     (lam (x ...) e)
-     (proc (x ...) e)
+     ;apply
      (app e (e ...))
+     ;apply with block
      (app-b e e)
      (ret e)
      (let ((x e) ...) e)
-     x) 
-  (addr string)
-  (sto ((addr e) ...))
-  (env ((x addr) ...))
-  (kont (x e env kont) 
-        halt)
+     x)
+  ;all expressions
+  (e ce
+     ae) 
+  ;configuration definitions
+  (sto ((x e) ...))
+  (env ((x x) ...))
+  (kont (k-do x e env kont) ;keep environment if do
+        (k-ret x e env kont) ;originating from a block call
+        (k x e env kont) ;normal control point 
+        halt) ;base case
   (CF (e env sto kont))
-  (x variable-not-otherwise-mentioned)
-)  
+  (x variable-not-otherwise-mentioned))  
+
+(define (rr x) (apply-reduction-relation rc-red x))
 
 (define rc-red
   (reduction-relation
-   ruby-core
+   ruby-core #:domain CF
+   ;final configuration
+   ;(--> (ae env sto halt)
    
-#|
-(define postfix-red
-  (reduction-relation
-   PFL #:domain CF
-   (--> ((N C ...) S)
-        ((C ...) (N . S))
-        PF-Num)
-   (--> ((Q C ...) S)
-        ((C ...) (Q . S))
-        PF-CmdSeq)
-   (--> ((pop C ...) (V_f V_r ...))
-        ((C ...) (V_r ...))
-        PF-Pop)
-   (--> ((swap C ...) (V_1 V_2 V_r ...))
-        ((C ...) (V_2 V_1 V_r ...))
-        PF-Swap)
-   (--> ((sel C ...) (V_f V_t 0 V_r ...))
-        ((C ...) (V_f V_r ...))
-        PF-SelFalse)
-   (--> ((sel C ...) (V_f V_t N V_r ...))
-        ((C ...) (V_t V_r ...))
-        (side-condition (not (zero? (term N))))
-        PF-SelTrue)
-   (--> ((exec C ...) ((C_Q ...) V_r ...))
-        ((C_Q ... C ...) (V_r ...))
-        PF-Exec)
-   (--> ((A C ...) (N_1 N_2 V_r ...))
-        ((C ...) (N_ans V_r ...))
-        (where N_ans (calculate A N_1 N_2))
-        PF-Arith)
-   (--> ((R C ...) (N_1 N_2 V_r ...))
-        ((C ...) (N_ans V_r ...))
-        (where N_ans (compare R N_1 N_2))
-        PF-Rel)
-   (--> ((nget C ...) (N_i V_r ...))
-        ((C ...) (V_ans V_r ...))
-        (side-condition (and (> (term N_i) 0) (<= (term N_i) (length (term (V_r ...))))))
-        (where V_ans ,(list-ref (term (V_r ...)) (sub1 (term N_i))))
-        PF-Nget)))
-|#
+   ;atomic expression - call current continuation
+   (--> (ae env sto (k x e env_k kont))
+        (e ((x addr) . env_k) ((addr ae) . sto) kont)
+        (where addr ,(gensym)))
+   (--> (ae env sto (k-do x e env_k kont))
+        (e ((x addr) . env) ((addr ae) . sto) kont)
+        (where addr ,(gensym)))
+                
+   ;id lookup
+   (--> (x env sto kont)
+        (ae_ans env sto kont)
+        ;(side-condition (member (term x) (term env)))
+        (where loc ,(assoc (term x) (term env)))
+        (where ae_ans ,(assoc (term loc) (term sto))))
+   
+   ;if 
+   (--> ((if #t e_t e_f) env sto kont)
+        (e_t env sto kont))   
+   (--> ((if #f e_t e_f) env sto kont)
+        (e_f env sto kont))
+   (--> ((if ce e_t e_f) env sto kont)
+        (ce env sto kont_new)
+        (where kont_new (gen-kont (if ce e_t e_f) env kont)))
+   
+   ;let            
+   (--> ((let ((x e)) e_body) env sto kont)
+        (e env sto (x e_body env kont)))   
+   (--> ((app e_f (e_1 ...)) env sto kont)
+        (e_f env sto kont_new)
+        (where kont_new (gen-kont (app e_f (e_1 ...)) env kont)))
+   
+   ;do
+   (--> ((do e_1 e_2 ...) env sto kont)
+        (e_1 env sto kont_new)
+        (where kont_new (gen-kont (do e_1 e_2 ...))))   
+   (--> ((do e) env sto kont)
+        (e env sto kont))
+
+   ))
+
+
+
+(define-metafunction ruby-core
+  gen-kont : e env kont -> kont  
+    [(gen-kont (app e_f (e_1 ...)) env kont)
+     ,(let ((s (gensym))) (term (k ,s (app ,s (e_1 ...)) env kont)))]
+    [(gen-kont (do e_1 e_2 ...) env kont)
+     ,(let ((s (gensym))) (term (k-do ,s (do e_1 e_2 ...) env kont)))]
+    [(gen-kont (if e e_t e_f) env kont)
+     ,(let ((s (gensym))) (term (k ,s (if ,s e_t e_f) env kont)))])
+
+
